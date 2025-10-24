4950ba4a6a1b79f02351c86722457fec680eac1e
Qaush Qestaj <qaushqestaj@hotmail.com>
2025-10-24 17:40:50 +0200

    first commit

---
+++ LeaveManagementSystem.Web/Data/LeaveType.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeaveManagementSystem.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string? Name { get; set; }
+        public int NumberOfDays { get; set; }
+    }
+}
